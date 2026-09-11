f9d68f9317ca32236eab84926b623b226c19ea8b
CourageGanyiwa <couresma@gmail.com>
2026-09-11 10:29:15 +0200

    added whitelist

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const Order = require("./models/Order");
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/coresupplements";
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || "http://localhost:5173";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: FRONTEND_ORIGIN }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
